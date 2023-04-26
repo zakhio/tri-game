75ade7e771545c661fe73b067747264c91510720
Andrey Zakharov <6272035+zakhio@users.noreply.github.com>
2023-04-26 20:59:16 +0200

    Backend: Adds feature when backend serves frontend (#3)
    To keep the system simple and not to have nodejs running in the same image or a separate image for frontend. Backend will serve static files and redirect game ids to the index.html

--- backend/src/main/kotlin/io/zakh/tri/WebConfig.kt
+++ backend/src/main/kotlin/io/zakh/tri/WebConfig.kt
@@ -1,18 +1,13 @@
 package io.zakh.tri
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-@EnableWebMvc
 class WebConfig : WebMvcConfigurer {
-    override fun addCorsMappings(registry: CorsRegistry) {
-
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:5173", "http://localhost:4000")
-            .allowCredentials(true)
-            .allowedMethods("*")
+    override fun addViewControllers(registry: ViewControllerRegistry) {
+        registry.addViewController("/{gameID:\\w*}")
+            .setViewName("index.html")
     }
 }
