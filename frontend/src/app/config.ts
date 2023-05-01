90c53c53ae66d5e433553099f14cf48a435bb884
Andrey Zakharov <6272035+zakhio@users.noreply.github.com>
2023-05-01 19:31:57 +0200

    Fixes REST Client base url on frontend (#13)

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -1,21 +1,3 @@
-export function hostUrl(): string {
-    const domainParts = window.location.hostname.split(".");
-    const tld = domainParts[domainParts.length - 1];
-    let serviceUrl;
-
-    // primitive check if last part of url is number then hostname is IP address
-    if (tld === parseInt(tld).toString()) {
-        serviceUrl = domainParts
-    } else {
-        // extracts only last two parts from host name
-        // for example, foo.bar.com -> bar.com or localhost -> localhost
-        const domainStart = Math.max(0, domainParts.length - 2);
-        serviceUrl = domainParts.slice(domainStart)
-    }
-
-    return window.location.protocol + '//' + serviceUrl.join(".") + ':8080';
-}
-
 export function gameSessionUrl(sessionId: string): string {
     return window.location.protocol + '//' + window.location.host + "/" + sessionId;
 }
